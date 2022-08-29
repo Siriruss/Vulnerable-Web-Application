e5a0c15ead41b2406540946b308bdbbfc10cb9d5
Noah Russaw <noah.russaw@yahoo.com>
2022-08-29 15:52:43 -0600

    Remove duplicate code from app.js

--- app.js
+++ app.js
@@ -52,23 +52,6 @@ async function main() {
   await mongoose.connect('mongodb://localhost:27017/UserDB');
 }
 
-const user1 = new User({
-  email: 'todd.gill@gmail.com',
-  password: 'Batman123!'
-})
-
-const user2 = new User({
-  email: 'emma.daniels@gmail.com',
-  password: 'ChicagoSKY12#$'
-})
-
-const user3 = new User({
-  email: 'admin',
-  password: 'Christmas1995'
-})
-
-const defaultUsers = [user1, user2, user3];
-
 require('./src/config/routes')(app);
 
 //sets the port for the server to run on
